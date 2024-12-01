5344f75c7bb1ce32139205cf4b04dda3fa9e7526
Zaid <zaid.ahmed@ucalgary.ca>
2024-11-30 17:56:08 -0700

    initial project setup

---
+++ Assets/DisplayCapture/Barcode/Indicator.cs
@@ -0,0 +1,46 @@
+using TMPro;
+using UnityEngine;
+
+namespace Anaglyph.DisplayCapture.Barcodes
+{
+	public class Indicator : MonoBehaviour
+	{
+		[SerializeField] private LineRenderer lineRenderer;
+		public LineRenderer LineRenderer => lineRenderer;
+
+		[SerializeField] private TMP_Text textMesh;
+		public TMP_Text TextMesh => textMesh;
+
+		private Vector3[] offsetPositions = new Vector3[4];
+
+		public void Set(BarcodeTracker.Result result) => Set(result.text, result.corners);
+
+		public void Set(string text, Vector3[] corners)
+		{
+			if (text.StartsWith("https://zaid-ahmed.me/"))
+			{
+				// Remove the prefix
+				text = text.Substring(22);
+				Vector3 topCenter = (corners[2] + corners[3]) / 2f;
+				transform.position = topCenter;
+
+				Vector3 up = (corners[1] - corners[0]).normalized;
+				Vector3 right = (corners[2] - corners[1]).normalized;
+				Vector3 normal = -Vector3.Cross(up, right).normalized;
+
+				Vector3 center = (corners[2] + corners[0]) / 2f;
+
+				for (int i = 0; i < 4; i++)
+				{
+					Vector3 dir = (corners[i] - center).normalized;
+					offsetPositions[i] = corners[i] + (dir * 0.1f);
+				}
+
+				transform.rotation = Quaternion.LookRotation(normal, up);
+
+				lineRenderer.SetPositions(offsetPositions);
+				textMesh.text = text;
+			}
+		}
+	}
+}
